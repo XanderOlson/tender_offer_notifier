0a061b7447926c32ce73d3dc4cf9e5b6a6566b15
Xander <AlexanderM.Olson@gmail.com>
2015-04-01 10:45:55 -0700

    add send email functionality send email only when new company added to list

--- tender_offer.py
+++ tender_offer.py
@@ -1,7 +1,6 @@
 """
 Python Tender Offer Notifier
 Alexander Olson
-Version 0.0.1 - 2015_04_01
 """
 
 from BeautifulSoup import BeautifulSoup
@@ -15,15 +14,17 @@ URL_PREFIX = 'https://www.sec.gov'
 try:
 	company_set = pickle.load( open('save.p', 'rb'))
 	SECRET = pickle.load( open('secret.p', 'rb'))
+	PERSONAL_EMAIL = pickle.load( open('personal_email.p', 'rb'))
 except:
 	company_set = set()
 	print 'Failed to load company_set and password'
-    sys.quit()
+	sys.exit()
+
 
 def main():
-	for i in xrange(1, 4):
+	for i in xrange(1, 5):
 		start_id = str(i)
-		url = 'https://www.sec.gov/cgi-bin/srch-edgar?text=sc%20to-i%2Fa&start='+ start_id +'&count=100&first=' + YEAR + '&last=' + YEAR
+		url = 'https://www.sec.gov/cgi-bin/srch-edgar?text=sc%20to-i&start='+ start_id +'&count=100&first=' + YEAR + '&last=' + YEAR
 
 		page = urllib2.urlopen(url)
 		soup = BeautifulSoup(page)
@@ -33,37 +34,38 @@ def main():
 			if str(e.string) not in company_set:
 				company_set.add(str(e.string))
 				print 'New Company'
-				# send_email()
+				message = str(e.string) + ' ' + URL_PREFIX + str(e.get('href'))
+				send_email(message)
 
 	
 	pickle.dump( company_set, open( 'save.p', 'wb') )
 	print len(company_set)
 
 
-def send_email():
-    import smtplib
-    gmail_user = "AlexanderM.Olson@gmail.com"
-    gmail_pwd = SECRET
-    FROM = 'AlexanderM.Olson@gmail.com'
-    TO = ['AlexanderM.Olson@gmail.com'] #must be a list
-    SUBJECT = "Testing sending using gmail"
-    TEXT = "Testing sending mail using gmail servers"
+def send_email(message):
+	import smtplib
+	gmail_user = PERSONAL_EMAIL
+	gmail_pwd = SECRET
+	FROM = PERSONAL_EMAIL
+	TO = [PERSONAL_EMAIL] #must be a list
+	SUBJECT = "New Company - Tender Offer Amendment"
+	TEXT = message
 
-    # Prepare actual message
-    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    try:
-        #server = smtplib.SMTP(SERVER) 
-        server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
-        server.ehlo()
-        server.starttls()
-        server.login(gmail_user, gmail_pwd)
-        server.sendmail(FROM, TO, message)
-        #server.quit()
-        server.close()
-        print 'successfully sent the mail'
-    except:
-        print "failed to send mail"
+	# Prepare actual message
+	message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+	""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+	try:
+		#server = smtplib.SMTP(SERVER) 
+		server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+		server.ehlo()
+		server.starttls()
+		server.login(gmail_user, gmail_pwd)
+		server.sendmail(FROM, TO, message)
+		#server.quit()
+		server.close()
+		print 'successfully sent the mail'
+	except:
+		print "failed to send mail"
 
 
 if __name__ == '__main__':
